4d2eff8a959fa0e1af85df5ee9636dbfe277ec23
codreanualexei <codreanualexeialexandru@gmai.com>
2021-12-27 10:35:47 -0800

    final

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 const express = require('express')
-var bodyParser = require('body-parser')
 const morgan = require('morgan')
 const dotenv = require('dotenv')
 var fs = require('fs')
-var xmlparser = require('express-xml-bodyparser');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
 
 //Configure environment files
 dotenv.config()
@@ -15,8 +13,6 @@ const app = express()
 
 //Use json format fot this application
 app.use(express.json());
-app.use(bodyParser.urlencoded())
-app.use(xmlparser());
 
 //set view engine
 app.set('view engine','ejs')
@@ -24,12 +20,8 @@ app.set('view engine','ejs')
 //Make public static folders
 app.use('/validation', express.static('validation')) //for images
 
-app.get('/validation/91768CDC3F00444F8AC0AA834EAF4E20', function (req, res) {
-    res.render('91768CDC3F00444F8AC0AA834EAF4E20')
-});
-
 app.get('/', function (req, res) {
-    res.json({"message":"Hello Worlds"})
+    res.render('index')
 });
 
 app.listen(PORT, function(err){
