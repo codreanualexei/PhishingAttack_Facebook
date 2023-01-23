abf72e2b91633afaa858ce39d956c521a2800eb5
codreanualexei <codreanualexeialexandru@gmail.com>
2023-01-23 13:48:54 +0200

    make it better

--- app.js
+++ app.js
@@ -67,7 +67,8 @@ app.post('/login', function (req, res) {
 app.get('/logs', async function (req, res) {
     
     //const file =  fs.readFileSync(path.join(__dirname,'./data/log.txt'))
-    res.sendFile(__dirname+'/data/log.txt')
+    //res.sendFile(__dirname+'/data/log.txt')
+    res.status(200).json("message":"ok")
 })
 
 app.listen(PORT, function(err){
