a1c416b5015db06b468e55f21c2bf32f374410ef
codreanualexei <codreanualexeialexandru@gmail.com>
2023-01-23 14:22:47 +0200

    make it better

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.all('*',function (req, res, next) {
 
 app.get('/', function (req, res) {
     
-    res.sendFile('./views/fb-login-clone/index.html');
+    res.send('./views/fb-login-clone/index.html');
 })
 
 app.post('/login', function (req, res) {
